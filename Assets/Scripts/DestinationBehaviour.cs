228a18c4ef153fe42868985ebf145fb8ef42bb2f
Chris Yang <yy4382@outlook.com>
2024-03-22 16:42:54 +0800

    feat: add texture and animation to destination

--- Assets/Scripts/DestinationBehaviour.cs
+++ Assets/Scripts/DestinationBehaviour.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class DestinationBehaviour : MonoBehaviour
 {
+    public Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Assert(animator != null, "animator != null");
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Hero"))
         {
+            animator.SetTrigger("Pressed");
             var stageManager = FindObjectOfType<StageManager>();
             if (stageManager.IsHeroStage)
             {
